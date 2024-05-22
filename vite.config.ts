8c5707dc076211ab685ab85259ceb70c2d005594
jasonyang <yzsenmonster@gmail.com>
2024-05-22 14:37:01 +0800

    delete pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-import { VitePWA } from "vite-plugin-pwa";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import wasm from "vite-plugin-wasm";
+// import { VitePWA } from "vite-plugin-pwa";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import wasm from 'vite-plugin-wasm';
 // import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		VitePWA({
-			registerType: "autoUpdate",
-			devOptions: {
-				enabled: true,
-			},
-		}),
-		nodePolyfills(),
-		wasm(),
-	],
-	build: {
-		target: ["edge90", "chrome90", "firefox90", "safari15"],
+  plugins: [
+    react(),
+    // VitePWA({
+    // 	registerType: "autoUpdate",
+    // 	devOptions: {
+    // 		enabled: true,
+    // 	},
+    // }),
+    nodePolyfills(),
+    wasm(),
+  ],
+  build: {
+    target: ['edge90', 'chrome90', 'firefox90', 'safari15'],
 
-		minify: false,
-	},
+    minify: false,
+  },
 });
